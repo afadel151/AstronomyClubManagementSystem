84c4559bf500a9456666ba60e6ca1dc4a5085cc3
Akram Fadel <af8356207@gmail.com>
2026-06-05 18:04:30 +0100

    mongodb context

---
+++ Data/Context/MongoDbContext.cs
@@ -0,0 +1,27 @@
+using Data.MongoDocuments;
+using MongoDB.Driver;
+
+namespace Data.MongoDB;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(IMongoClient client, string databaseName)
+    {
+        _database = client.GetDatabase(databaseName);
+    }
+
+    // One typed collection per document type
+    public IMongoCollection<ObservationDetail> ObservationDetails =>
+        _database.GetCollection<ObservationDetail>("observation_detail");
+
+    public IMongoCollection<ImageDocument> ImageDocuments =>
+        _database.GetCollection<ImageDocument>("image_document");
+
+    // public IMongoCollection<EquipmentSpec> EquipmentSpecs =>
+    //     _database.GetCollection<EquipmentSpec>("equipment_specs");
+
+    // public IMongoCollection<ForecastPlan> ForecastPlans =>
+    //     _database.GetCollection<ForecastPlan>("forecast_plan");
+}
