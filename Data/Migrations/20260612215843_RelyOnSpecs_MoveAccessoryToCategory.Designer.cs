a8c01746674a80b2847a41684785fa12219a0c8f
Akram Fadel <af8356207@gmail.com>
2026-06-12 22:59:30 +0100

    Migration: RelyOnSpecs_MoveAccessoryToCategory

--- Data/Migrations/20260612215843_RelyOnSpecs_MoveAccessoryToCategory.Designer.cs
+++ Data/Migrations/20260612215843_RelyOnSpecs_MoveAccessoryToCategory.Designer.cs
@@ -4,6 +4,7 @@ using Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(AstroClubDbContext))]
-    partial class AstroClubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260612215843_RelyOnSpecs_MoveAccessoryToCategory")]
+    partial class RelyOnSpecs_MoveAccessoryToCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20260612215843_RelyOnSpecs_MoveAccessoryToCategory.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelyOnSpecs_MoveAccessoryToCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Accessory",
+                table: "EquipmentModels");
+
+            migrationBuilder.DropColumn(
+                name: "OpticalDesign",
+                table: "EquipmentModels");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDedicated",
+                table: "EquipmentCompatibilities",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Accessory",
+                table: "EquipmentCategories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SpecsType",
+                table: "EquipmentCategories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDedicated",
+                table: "EquipmentCompatibilities");
+
+            migrationBuilder.DropColumn(
+                name: "Accessory",
+                table: "EquipmentCategories");
+
+            migrationBuilder.DropColumn(
+                name: "SpecsType",
+                table: "EquipmentCategories");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Accessory",
+                table: "EquipmentModels",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OpticalDesign",
+                table: "EquipmentModels",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
