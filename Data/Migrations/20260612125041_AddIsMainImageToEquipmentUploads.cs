de7e3497d24ca697f67b832aacea50ef67e2c31d
Akram Fadel <af8356207@gmail.com>
2026-06-12 14:08:52 +0100

    Add isMainImage bool to EquipmentUploads

---
+++ Data/Migrations/20260612125041_AddIsMainImageToEquipmentUploads.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsMainImageToEquipmentUploads : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMainImage",
+                table: "EquipmentUploads",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMainImage",
+                table: "EquipmentUploads");
+        }
+    }
+}
