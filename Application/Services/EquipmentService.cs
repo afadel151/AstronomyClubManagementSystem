c47ab5cbb16ddcf1c495f45d4756d82d4f8385fb
Akram Fadel <af8356207@gmail.com>
2026-06-12 13:14:32 +0100

    setup blazor pages with Route parameter

---
+++ Application/Services/EquipmentService.cs
@@ -0,0 +1,15 @@
+using Application.Repositories;
+using Data.Entities.Generated;
+
+
+namespace Application.Services;
+
+public interface IEquipmentService
+{
+
+}
+
+public sealed class EquipmentService() : IEquipmentService
+{
+    
+}
