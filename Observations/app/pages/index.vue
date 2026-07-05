08a19fc021451b721a127e508901f61bae8f506e
Akram Fadel <af8356207@gmail.com>
2026-07-05 14:06:38 +0100

    setup the nuxt app with shadcn-vue

---
+++ Observations/app/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Button from '~/components/ui/button/Button.vue';
+</script>
+
+
+<template>
+
+    <div>
+        <Button>Hello</Button>
+    </div>
+</template>
--- Observations/nuxt.config.ts
+++ Observations/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ['shadcn-nuxt'],
 })
